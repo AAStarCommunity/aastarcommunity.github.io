4f36398da7b66adb21911cb7710d722b89856a20
jhfnetboy <jhfnetboy@gmail.com>
2025-10-17 12:22:41 +0700

    update links

--- components/subdomain-links.tsx
+++ components/subdomain-links.tsx
@@ -115,6 +115,7 @@ const SubdomainLinks: React.FC = () => {
         </div>
 
         <div>
+          <h3 className="text-2xl font-semibold text-blue-800 mb-6">Tools</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {subdomains.tools.map((item) => (
               <Link
